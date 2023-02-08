53d45cf445e85f4c6f7e58005643bea3730a4c1f
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-08 12:35:52 +0100

    htmlGerüst

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
 console.log("test");
+
+/* 
+ausgabe in html mit write()
+
+HTML Formular
+    Name
+    Alter
+    Prüfung bestanden ?
+    Button Person
+
+beim clicken soll
+    neues Objekt erstellt werden
+    und write() Methode aufgerufen
+
+    
+    wenn If    nicht    bestanden     
+        dann Name color = red
+
+*/
+
+
+
